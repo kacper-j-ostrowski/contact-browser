13af26f4e24183c9056696c037bba0a35011c783
Kacper Ostrowski <kacper.j.ostrowski@gmail.com>
2018-06-06 23:35:21 +0200

    sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into roles values ('ROLE_ADMIN', "Administrator");
+insert into roles values ('ROLE_USER', "Uzytkownik");
+
+insert into users values (1, "Kacper", "Ostrowski", "admin", "admin", "ROLE_ADMIN");
+insert into users values (2, "Kajtek", "Ostrowski", "user", "user", "ROLE_ADMIN");
