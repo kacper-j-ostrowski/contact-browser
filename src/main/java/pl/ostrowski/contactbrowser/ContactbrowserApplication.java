ef8fa0453bf82a27a6161e722531330435ce7c07
Kacper Ostrowski <kjo@netcompany.com>
2018-06-07 09:19:59 +0200

    configures database initialization

--- src/main/java/pl/ostrowski/contactbrowser/ContactbrowserApplication.java
+++ src/main/java/pl/ostrowski/contactbrowser/ContactbrowserApplication.java
@@ -1,11 +1,8 @@
 package pl.ostrowski.contactbrowser;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import pl.ostrowski.contactbrowser.controllers.CategoryRestController;
 
 @SpringBootApplication
 @EnableCaching
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into roles values ('ROLE_ADMIN', "Administrator");
-insert into roles values ('ROLE_USER', "Uzytkownik");
+insert into roles (code, name) values ('ROLE_ADMIN', 'Administrator');
+insert into roles (code, name) values ('ROLE_USER', 'Uzytkownik');
 
-insert into users values (1, "Kacper", "Ostrowski", "admin", "admin", "ROLE_ADMIN");
-insert into users values (2, "Kajtek", "Ostrowski", "user", "user", "ROLE_ADMIN");
+insert into users (id, firstname, lastname, username, password, role_code) values (1, 'Kacper', 'Ostrowski', 'admin', 'admin', 'ROLE_ADMIN');
+insert into users (id, firstname, lastname, username, password, role_code) values (2, 'Kajtek', 'Ostrowski', 'user', 'user', 'ROLE_ADMIN');
