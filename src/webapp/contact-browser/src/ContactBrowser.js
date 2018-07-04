a93238f7b5e90a66dc2ca49a4d01514e44f7b798
Kacper Ostrowski <kacper.j.ostrowski@gmail.com>
2018-07-04 22:43:59 +0200

    added CategoryPanel.js

--- src/webapp/contact-browser/src/ContactBrowser.js
+++ src/webapp/contact-browser/src/ContactBrowser.js
@@ -51,34 +51,10 @@ class ContactBrowser extends Component {
     };
 
     return(
-      <AppFrame>
-        <div>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="headline" component="h2">
-                Lizard
-              </Typography>
-              <Typography component="p">
-                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
-            </CardActions>
-          </Card>
-        </div>
-      </AppFrame>
+      <div>
+        <AppFrame/>
+        <CategoryPanel/>
+      </div>
     );
   }
 }
